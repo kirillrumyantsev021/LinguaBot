8dddeabe33d86f7669ea1838b404464de6340114
kirillrumyantsev021 <kirillrumancev088@gmail.com>
2025-05-07 18:17:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import asyncio
 import logging
 from create_bot import bot, dp
-from handlers.Oxford_api import oxford_router
+from handlers.define import define_router
 from handlers.cards import cards_router
-
+from handlers.goal import goal_router
 
 logging.basicConfig(level=logging.INFO)
 
@@ -11,7 +11,8 @@ async def main():
     # dp.middleware.setup(LoggingMiddleware())
     
     dp.include_router(cards_router)
-    dp.include_router(oxford_router)
+    dp.include_router(define_router)
+    dp.include_router(goal_router)
     
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
